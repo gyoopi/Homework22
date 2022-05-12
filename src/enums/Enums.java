518d4f3dd578f746905aa774e7678f517052a934
Shamil Zainetdinov <shamilzainetdinov@yandex.ru>
2022-05-12 21:39:02 +0700

    init commit Homework22

---
+++ src/enums/Enums.java
@@ -0,0 +1,17 @@
+package enums;
+
+public class Enums {
+
+    public enum Sex {
+        MAN,
+        WOMAN
+    }
+
+    public enum Education {
+        ELEMENTARY,
+        SECONDARY,
+        FURTHER,
+        HIGHER
+    }
+
+}
